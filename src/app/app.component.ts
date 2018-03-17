448b0ba11c397bf9a7aff14ed3d9ce0cbd84c92e
Guy Warburg <guy.warburg@gmail.com>
2018-03-17 22:24:28 +0200

    clean data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<snake-wrapper></snake-wrapper>`
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
